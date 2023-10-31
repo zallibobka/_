a24c26d4ee90045c26625284de1d4b485fd9f857
zallibobka <149335278+zallibobka@users.noreply.github.com>
2023-10-31 12:06:11 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-import kivy
-kivy.require('1.0.7')
-
 from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager , Screen
 
+class MenuScreen(Screen) :
+    def __init__(self,**kwargs) :
+        super(MenuScreen,self).__init__(**kwargs)
+        self.fl = BoxLayout(orientation = "vertical")
+        #Кнопка на которую нужно нажать
+        self.but_open = Button (text = "нажми на меня",size_hint = (None,None) ,size = (600,40),pos = (20,50),on_press = self.ButtonOpen)
+        self.fl.add_widget(self.but_open)
 
-class TestApp(App):
-    pass
+     def ButtonOpen(self):
+         self.but_open1 = Button (text = "кнопка1",size_hint = (None,None) ,size = (600,40),pos = (20,100))
+         self.but_open2 = Button (text = "Не жми на меня",size_hint = (None,None) ,size = (600,40),pos = (20,150))
+         self.fl.add_widget(self.but_open1)
+         #Аналогично пишем сюда и другие кнопки
 
+class MyApp() :
+    def build(self):
+        manager = ScreenManager()
+    	manager.add_widget(MenuScreen(name = 'menu'))
+    	return manager
 
-if __name__ == '__main__':
-    TestApp().run()
+if __name__ == '__main__' :
+    MyApp().run()
